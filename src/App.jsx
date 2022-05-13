84720903471b1d4bf703054620e333513009af9c
hiro8watanabe <87076032+hiro8watanabe@users.noreply.github.com>
2022-05-14 02:33:55 +0900

    style-comp

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,40 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="input-area">
+        <input type="text" placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
+        <ul>
+          <li className="flex-box">
+            <p>ああああああああ</p>
+            <button>完了</button>
+            <button>削除</button>
+          </li>
+          <li className="flex-box">
+            <p>ああああああああ</p>
+            <button>完了</button>
+            <button>削除</button>
+          </li>
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了したTODO</p>
+        <ul>
+          <li className="flex-box">
+            <p>ああああああああ</p>
+            <button>戻す</button>
+          </li>
+          <li className="flex-box">
+            <p>ああああああああ</p>
+            <button>戻す</button>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 };
